3ac59e60a3ed387349f98c675896e438863da4dd
hanifullah313 <hu4206835@gmail.com>
2025-08-14 04:27:01 -0700

    Netflix Data visualization capstone project completed successfully

--- Histgram.py
+++ Histgram.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-
 #example of histogram 
 #please mixed the data of score with good distribution 
 score = [1,2,3,44,23,24,43,54,64,63,63,8,62,41,34,63,42,62,8,7,45,45,82,90,88]
--- introduction/basic.py
+++ introduction/basic.py
@@ -1,8 +1,5 @@
 import matplotlib.pyplot as plt
-
 x= [1,2,3,4,5]
 y= [6,4,6,2,4]
-
 plt.plot(x,y)
-
 plt.show()
